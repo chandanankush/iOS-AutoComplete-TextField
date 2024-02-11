cc361810b6a9fc833458f1ba0733fde0fe742fd4
Chandan Singh <chandan.ankush@gmail.com>
2024-02-11 22:16:00 +0530

    interim commit

---
+++ Chandan_Controls/Chandan_Controls/ExampleViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Chandan_Controls
+//
+//  Created by Gudia on 11/02/24.
+//  Copyright © 2024 Chandan SIngh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CAAutoFillTextField
+
+class ExampleViewController: UIViewController {
+    
+    @IBOutlet private weak var myTextField: CAAutoFillTextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var tempArray: [CAAutoCompleteObject] = []
+        for i in 0...10 {
+            let object = CAAutoCompleteObject(objName: "drop down \(i)", AndID: i)
+            tempArray.append(object)
+        }
+        myTextField.dataSourceArray = tempArray
+        myTextField.delegate = self
+    }
+}
+
+extension ExampleViewController: CAAutoFillDelegate {
+
+    func caAutoTextFillBeginEditing() {
+        
+    }
+
+    func caAutoTextFillEndEditing() {
+        
+    }
+
+    func caAutoTextCanEdit() -> Bool {
+        return true
+    }
+}
